d0e4f28be43549b915d4c90d574583380a7aa79e
Iyatan Atchoro <iyatanatchoro@Iyatans-MacBook-Air.local>
2019-01-21 23:02:43 -0500

    Updates

--- precache-manifest.04af710d00999438391f77a50aefcf18.js
+++ precache-manifest.04af710d00999438391f77a50aefcf18.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "8303e3175a41c03dcad8",
+    "revision": "cda8051a101b09b6ade0",
     "url": "/Toronto_Waste_LookUp/static/css/main.82a1c039.chunk.css"
   },
   {
-    "revision": "8303e3175a41c03dcad8",
-    "url": "/Toronto_Waste_LookUp/static/js/main.8303e317.chunk.js"
+    "revision": "cda8051a101b09b6ade0",
+    "url": "/Toronto_Waste_LookUp/static/js/main.cda8051a.chunk.js"
   },
   {
     "revision": "c42c691c27529fb8e0b2",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/Toronto_Waste_LookUp/static/js/runtime~main.4a846187.js"
   },
   {
-    "revision": "45772c1da4745b53fe14e534a1591a18",
+    "revision": "99343996858a82ed1608aaed127b08e6",
     "url": "/Toronto_Waste_LookUp/index.html"
   }
 ];
